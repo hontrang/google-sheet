0be386b1167f1b76ca9df998444ba29a367c8c83
hon.trang <hon.trang@isobar.com>
2023-03-01 09:49:37 +0700

    update

--- sheetUtility.js
+++ sheetUtility.js
@@ -1,9 +1,7 @@
 var SheetUtility = {
   ghiDuLieuVaoDay: function (data, sheetName, row, column) {
     let sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
-    // xoá trắng range
     sheet.getRange(row, column, data.length, data[0].length).clearContent();
-
     sheet.getRange(row, column, data.length, data[0].length).setValues(data);
   },
   ghiDuLieuVaoO: function (data, sheetName, cell) {
@@ -14,11 +12,7 @@ var SheetUtility = {
   },
   layGiaTriTheoCot: function (activeSheet, rowIndex, columnIndex) {
     let sheet = SpreadsheetApp.getActive().getSheetByName(activeSheet);
-    range = sheet.getRange(
-      rowIndex,
-      columnIndex,
-      sheet.getLastRow() - rowIndex + 1
-    );
+    range = sheet.getRange(rowIndex, columnIndex, sheet.getLastRow() - rowIndex + 1);
     // xoá phần tử rỗng trong mảng
     return range.getValues().filter(function (el) {
       return el != "";
