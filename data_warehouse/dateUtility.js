13a188bbe8ed68bebc8baf60157583f67f092438
hon.trang <hon.trang@isobar.com>
2023-07-09 20:00:53 +0700

    refactor

--- data_warehouse/dateUtility.js
+++
@@ -1,6 +0,0 @@
-var SheetDate = {
-    getDate: function (number) {
-        let date = new Date(number);
-        return (date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate());
-    }
-}
