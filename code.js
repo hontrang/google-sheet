dee786d87049397cefc6cd8ad12e94c6ec450f12
hon.trang <hon.trang@isobar.com>
2023-07-10 17:27:22 +0700

    nâng cấp

--- code.js
+++ code.js
@@ -1,17 +1,20 @@
 function layChiSoVnIndex() {
   const ngayHienTai = moment().format("YYYY-MM-DD");
   const duLieuNgayMoiNhat = SheetUtility.layDuLieuTrongO("HOSE", "A1");
+  const thanhKhoanMoiNhat = SheetUtility.layDuLieuTrongO("HOSE", "D1");
   const url = "https://wgateway-iboard.ssi.com.vn/graphql";
 
   const query = "query indexQuery($indexIds: [String!]!) {indexRealtimeLatestByArray(indexIds: $indexIds) {indexID indexValue allQty allValue totalQtty totalValue advances declines nochanges ceiling floor change changePercent ratioChange __typename } }";
   const variables = `{"indexIds": ["VNINDEX" ]}`;
   const object = SheetHttp.sendGraphQLRequest(url, query, variables);
   const duLieuNhanVe = object.data.indexRealtimeLatestByArray[0];
-
-  if (duLieuNgayMoiNhat !== ngayHienTai) {
+  const thanhKhoan = duLieuNhanVe.totalValue * 1000000;
+  console.log(duLieuNgayMoiNhat !== ngayHienTai);
+  console.log(thanhKhoan !== thanhKhoanMoiNhat);
+  if (duLieuNgayMoiNhat !== ngayHienTai && thanhKhoan !== thanhKhoanMoiNhat) {
     SheetUtility.chen1HangVaoDauSheet("HOSE");
   }
-  const mang_du_lieu_chinh = [[ngayHienTai, duLieuNhanVe.indexValue, duLieuNhanVe.changePercent / 100, duLieuNhanVe.totalValue * 1000000]];
+  const mang_du_lieu_chinh = [[ngayHienTai, duLieuNhanVe.indexValue, duLieuNhanVe.changePercent / 100, thanhKhoan]];
 
   SheetUtility.ghiDuLieuVaoDayTheoTen(mang_du_lieu_chinh, "HOSE", 1, "A");
 }
