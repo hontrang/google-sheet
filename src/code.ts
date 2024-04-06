051012d5cab5e786e91a5751339a997711849aac
hon.trang <hon.trang@isobar.com>
2024-04-06 22:36:58 +0700

    feat: thêm rollup để xử lý export require - inprogress

--- src/code.ts
+++ src/code.ts
@@ -1,23 +1,20 @@
-import { DateUtil } from "./utility/dateUtil";
 import { SheetLog } from "./utility/logUtil";
-
-// Giả định về các module và kiểu
-declare const SheetUtility: any;
-declare const Cheerio: any;
-declare const SheetHttp: any;
-declare const ChartUtil: any;
+import { SheetUtil } from "./utility/sheetUtil";
+import { SheetHttp } from "./utility/httpUtil"; 
+import { ChartUtil } from "./utility/chartUtil";
+import * as Cheerio from 'cheerio';
 
 function getDataHose(): void {
-  const DANH_SACH_MA: string[] = SheetUtility.layDuLieuTrongCot(SheetUtility.SHEET_DU_LIEU, "A");
+  const DANH_SACH_MA: string[] = SheetUtil.layDuLieuTrongCot(SheetUtil.SHEET_DU_LIEU, "A");
   const URL: string = `https://bgapidatafeed.vps.com.vn/getliststockdata/${DANH_SACH_MA.join(",")}`;
   const response = SheetHttp.sendGetRequest(URL);
   const stockData = response.map(({ sym, lastPrice }: { sym: string; lastPrice: number; }) => [sym, lastPrice * 1000]);
-  SheetUtility.ghiDuLieuVaoDayTheoTen(stockData, SheetUtility.SHEET_DU_LIEU, 2, "B");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(stockData, SheetUtil.SHEET_DU_LIEU, 2, "B");
 }
 
 function layTinTucSheetBangThongTin(): void {
   const mangDuLieuChinh: Array<[string, string, string, string, string, string]> = [];
-  SheetUtility.layDuLieuTrongCot(SheetUtility.SHEET_CAU_HINH, "E").forEach((tenMa: string) => {
+  SheetUtil.layDuLieuTrongCot(SheetUtil.SHEET_CAU_HINH, "E").forEach((tenMa: string) => {
     const url: string = `https://s.cafef.vn/Ajax/Events_RelatedNews_New.aspx?symbol=${tenMa}&floorID=0&configID=0&PageIndex=1&PageSize=10&Type=2`;
     const content: string = SheetHttp.sendGetRequest(url).getContentText();
     const $ = Cheerio.load(content);
@@ -28,12 +25,12 @@ function layTinTucSheetBangThongTin(): void {
       mangDuLieuChinh.push([tenMa, title, "", link, "", date]);
     });
   });
-  SheetUtility.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtility.SHEET_BANG_THONG_TIN, 35, "A");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtil.SHEET_BANG_THONG_TIN, 35, "A");
 }
 
 function layThongTinChiTietMa(): void {
-  SheetUtility.ghiDuLieuVaoO("...", SheetUtility.SHEET_CHI_TIET_MA, "J2");
-  const tenMa: string = SheetUtility.layDuLieuTrongO(SheetUtility.SHEET_CHI_TIET_MA, "F1");
+  SheetUtil.ghiDuLieuVaoO("...", SheetUtil.SHEET_CHI_TIET_MA, "J2");
+  const tenMa: string = SheetUtil.layDuLieuTrongO(SheetUtil.SHEET_CHI_TIET_MA, "F1");
 
   layGiaVaKhoiLuongTheoMaChungKhoan(tenMa);
   // layBaoCaoPhanTich(tenMa);
@@ -44,21 +41,21 @@ function layThongTinChiTietMa(): void {
   // layThongTinCoDong(tenMa);
 
   ChartUtil.updateChart();
-  SheetLog.logTime(SheetUtility.SHEET_CHI_TIET_MA, "J2");
+  SheetLog.logTime(SheetUtil.SHEET_CHI_TIET_MA, "J2");
   Logger.log("Hàm layThongTinChiTietMa chạy thành công");
 }
 
 
 function layGiaVaKhoiLuongTheoMaChungKhoan(tenMa: string): void {
-  const fromDate: string = SheetUtility.layDuLieuTrongO(SheetUtility.SHEET_CHI_TIET_MA, "F2");
-  const toDate: string = SheetUtility.layDuLieuTrongO(SheetUtility.SHEET_CHI_TIET_MA, "H2");
+  const fromDate: string = SheetUtil.layDuLieuTrongO(SheetUtil.SHEET_CHI_TIET_MA, "F2");
+  const toDate: string = SheetUtil.layDuLieuTrongO(SheetUtil.SHEET_CHI_TIET_MA, "H2");
 
   const URL: string = `https://finfo-api.vndirect.com.vn/v4/stock_prices?sort=date&q=code:${tenMa}~date:gte:${fromDate}~date:lte:${toDate}&size=1000`;
   const object = SheetHttp.sendGetRequest(URL);
   const mangDuLieuChinh: Array<[string, number, number]> = object.data.map(
     ({ date, close, nmVolume }: { date: string; close: number; nmVolume: number; }) => [date, close * 1000, nmVolume]
   );
-  SheetUtility.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtility.SHEET_DU_LIEU, 2, "P");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtil.SHEET_DU_LIEU, 2, "P");
 }
 
 function layTinTucSheetChiTietMa(tenMa: string): void {
@@ -74,12 +71,12 @@ function layTinTucSheetChiTietMa(tenMa: string): void {
     const date: string = $(this).siblings("span").text().substr(0, 10);
     mangDuLieuChinh.push([tenMa.toUpperCase(), title, link, date]);
   });
-  SheetUtility.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtility.SHEET_DU_LIEU, 2, "AH");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtil.SHEET_DU_LIEU, 2, "AH");
 }
 
 function layBaoCaoTaiChinh(): void {
   const mang_du_lieu_chinh: Array<[string, string, string, string]> = [];
-  const tenMa: string = SheetUtility.layDuLieuTrongO(SheetUtility.SHEET_CHI_TIET_MA, "F1");
+  const tenMa: string = SheetUtil.layDuLieuTrongO(SheetUtil.SHEET_CHI_TIET_MA, "F1");
   const QUERY_URL: string = `https://s.cafef.vn/Ajax/CongTy/BaoCaoTaiChinh.aspx?sym=${tenMa}`;
   const content: string = UrlFetchApp.fetch(QUERY_URL).getContentText();
   const $ = Cheerio.load(content);
@@ -89,14 +86,14 @@ function layBaoCaoTaiChinh(): void {
     const link: string = $(this).children("td:nth-child(3)").children("a").attr("href") || ""; // Sử dụng giá trị mặc định để tránh undefined
     mang_du_lieu_chinh.push([tenMa.toUpperCase(), title, date, link]);
   });
-  SheetUtility.ghiDuLieuVaoDayTheoTen(mang_du_lieu_chinh.slice(1, 11), SheetUtility.SHEET_DU_LIEU, 18, "AH");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(mang_du_lieu_chinh.slice(1, 11), SheetUtil.SHEET_DU_LIEU, 18, "AH");
 }
 
 function layBaoCaoPhanTich(tenMa: string): void {
   const url: string = `https://edocs.vietstock.vn/Home/Report_ReportAll_Paging?xml=Keyword:${tenMa}&pageIndex=1&pageSize=9`;
   const object = SheetHttp.sendPostRequest(url); // Giả định về cấu trúc và kiểu dữ liệu của object
   const mangDuLieuChinh = object.Data.ReportData.map(({ SourceName, Title, ReportTypeName, LastUpdate, Url }: { SourceName:string, Title:string, ReportTypeName:string, LastUpdate: string, Url:string }) => [SourceName, Title, ReportTypeName, LastUpdate, Url]);
-  SheetUtility.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtility.SHEET_DU_LIEU, 2, "AL");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtil.SHEET_DU_LIEU, 2, "AL");
 }
 
 function layThongTinCoDong(tenMa: string): void {
@@ -107,5 +104,5 @@ function layThongTinCoDong(tenMa: string): void {
     ({ ownershiptypecode, name, percentage, quantity, publicdate }: { ownershiptypecode: string; name: string; percentage: string; quantity: string; publicdate: string }) => [ownershiptypecode, name, percentage, quantity, publicdate]
   );
 
-  SheetUtility.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtility.SHEET_DU_LIEU, 2, "AC");
+  SheetUtil.ghiDuLieuVaoDayTheoTen(mangDuLieuChinh, SheetUtil.SHEET_DU_LIEU, 2, "AC");
 }
--- src/utility/logUtil.ts
+++ src/utility/logUtil.ts
@@ -1,29 +1,29 @@
-import moment from 'moment'; 
+import moment from 'moment';
 
-declare const SpreadsheetApp: any; 
-declare const SheetUtility: any; 
+declare const SpreadsheetApp: any;
+declare const SheetUtility: any;
 
 const SheetLog = {
-    logDebug: function (content: any): void {
-      const data: [string, string][] = [
-        [moment().format("YYYY/MM/DD HH:mm:ss"), content.toString()]
-      ];
-      const sheet = SpreadsheetApp.getActive().getSheetByName(SheetUtility.SHEET_DEBUG);
-      sheet.getRange(sheet.getLastRow() + 1, 1, data.length, data[0].length).setValues(data);
-      Logger.log(content);
-    },
-    logTime: function (sheetName: string, cell: string): void {
-      SpreadsheetApp.getActive()
-        .getSheetByName(sheetName)
-        .getRange(cell)
-        .setValue(moment().format("YYYY/MM/DD HH:mm:ss"));
-    },
-  
-    logStart: function (sheetName: string, cell: string): void {
-      SpreadsheetApp.getActive()
-        .getSheetByName(sheetName)
-        .getRange(cell)
-        .setValue("...");
-    }
-  };
-  export {SheetLog}
+  logDebug: function (content: any): void {
+    const data: [string, string][] = [
+      [moment().format("YYYY/MM/DD HH:mm:ss"), content.toString()]
+    ];
+    const sheet = SpreadsheetApp.getActive().getSheetByName(SheetUtility.SHEET_DEBUG);
+    sheet.getRange(sheet.getLastRow() + 1, 1, data.length, data[0].length).setValues(data);
+    Logger.log(content);
+  },
+  logTime: function (sheetName: string, cell: string): void {
+    SpreadsheetApp.getActive()
+      .getSheetByName(sheetName)
+      .getRange(cell)
+      .setValue(moment().format("YYYY/MM/DD HH:mm:ss"));
+  },
+
+  logStart: function (sheetName: string, cell: string): void {
+    SpreadsheetApp.getActive()
+      .getSheetByName(sheetName)
+      .getRange(cell)
+      .setValue("...");
+  }
+};
+export { SheetLog }
