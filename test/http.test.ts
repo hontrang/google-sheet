8cc708c41f607674ef341040bf9bd44993c86364
hon.trang <hon.trang@isobar.com>
2024-04-16 20:58:26 +0700

    feat: lấy thông tin tỷ giá từ google finance

--- test/http.test.ts
+++ test/http.test.ts
@@ -1,13 +1,6 @@
-const A = "111";
-console.log(A);
-
-
-
 describe("Validate url vndirect works correctly", () => {
     test("it should validate response api vndirect", () => {
         const tenMa = "HPG";
-        // const fromDate = moment().format("YYYY-MM-DD");
-        // const toDate = moment().format("YYYY-MM-DD");
         const fromDate = "2024-04-08";
         const toDate = "2024-04-08";
         const URL = `https://finfo-api.vndirect.com.vn/v4/stock_prices?sort=date&q=code:${tenMa}~date:gte:${fromDate}~date:lte:${toDate}&size=1000`;
