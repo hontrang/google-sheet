2ffb406ea96210fea4a7c8841b0a54b5275c6651
hon.trang <trang1392@gmail.com>
2024-05-04 18:28:33 +0700

    feat: thêm eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,35 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-      },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+            },
+        },
     },
-  },
-  {
-    files: ['**/*.ts'],
-    ...tseslint.configs.disableTypeChecked,
-  }, {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    {
+        files: ['**/*.ts'],
+        ...tseslint.configs.disableTypeChecked,
     },
-  },
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            "@typescript-eslint/array-type": "off",
+            "@typescript-eslint/no-namespace": "off"
+        }
+    }
 );
