87603c3f1d5b5bce0f3a9677dd3591a19524cad4
hontrang <tang11392@gmail.com>
2024-08-03 23:01:04 +0700

    chore: fix eslint

--- src/utility/LogHelper.ts
+++ src/utility/LogHelper.ts
@@ -1,8 +1,8 @@
-namespace SheetLog {
+namespace LogHelper {
   export function logDebug(content: string): boolean {
     const data: [string, string][] = [[moment().format('YYYY/MM/DD HH:mm:ss'), content.toString()]];
     Logger.log(content);
-    const sheet = SpreadsheetApp.getActive().getSheetByName(SheetUtil.SHEET_DEBUG);
+    const sheet = SpreadsheetApp.getActive().getSheetByName(SheetHelper.SHEET_DEBUG);
     if (!sheet) return false;
     else {
       sheet.getRange(sheet.getLastRow() + 1, 1, data.length, data[0].length).setValues(data);
