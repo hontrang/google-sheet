be9e67b07bef3a128b36379ea392851a43585b9c
hontrang <tang11392@gmail.com>
2024-08-14 10:34:02 +0700

    fix: cập nhật

--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 export class Configuration {
-    static readonly consumerID = process.env.CONSUMERID,
+    static readonly consumerID = process.env.CONSUMERID
     static readonly consumerSecret = process.env.CONSUMERSECRET
     static readonly mode = 'offline';
     static readonly xlsxInput = "./dw.xlsx";
