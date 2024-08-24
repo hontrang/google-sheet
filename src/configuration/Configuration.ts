24dd717bd5eaaec08c5553ce7f9acbf44b1725d2
hontrang <tang11392@gmail.com>
2024-08-24 13:41:56 +0700

    fix: wrap bởi playwright framework

--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 export class Configuration {
-    static readonly consumerID = process.env.CONSUMERID
-    static readonly consumerSecret = process.env.CONSUMERSECRET
+    static readonly consumerID = '35e723ae7e4c426694226ed4649379d5'
+    static readonly consumerSecret = 'a54573a4a78949a0b59befc59523aee2'
     static readonly mode = 'offline';
-    static readonly xlsxInput = "./dw.xlsx";
-    static readonly xlsxOutput = "./build/dw_new.xlsx";
+    static readonly xlsxInput = './dw.xlsx';
+    static readonly xlsxOutput = './build/dw_new.xlsx';
+    static readonly dwClasp = '/Users/hontrang/code/clasp/dw';
+    static readonly mainClasp = '/Users/hontrang/code/clasp/main';
 }
---
+++ tests/offline.test.ts
@@ -0,0 +1,51 @@
+import { test } from '@playwright/test';
+import { ExcelHelper } from '@src/offline/ExcelHelper';
+import { AxiosHelper } from '@src/offline/AxiosHelper';
+import { SheetHelper } from '@src/utility/SheetHelper';
+import { ResponseVndirect } from '@src/types/types';
+
+const sheetHelper = new ExcelHelper();
+const httpHelper = new AxiosHelper();
+
+test('crawl data', async () => {
+  await sheetHelper.truyCapWorkBook();
+  const headers: string[] = sheetHelper.layDuLieuTrongHang(SheetHelper.sheetName.sheetKhoiNgoaiMua, 1).slice(1);
+  const dates = sheetHelper.layDuLieuTrongCot('TRUY VAN', 'A');
+  for (let i = 0; i < dates.length; i++) {
+    const date = dates[i];
+    await layThongTin(headers, date, i + 2);
+  }
+  await sheetHelper.luuWorkBook();
+});
+
+async function layThongTin(headers: string[], date: string, hang: number): Promise<void> {
+  const URL = `https://finfo-api.vndirect.com.vn/v4/stock_prices?size=1000&sort=date&q=code:${headers.join(',')}~date:gte:${date}~date:lte:${date}`;
+  const response = await httpHelper.sendGetRequest(URL);
+  Bun.sleepSync(100);
+  const datas = response.data.data;
+  const khoiLuong: string[] = [date];
+  if (datas.length > 0) {
+    for (const head of headers) {
+      const element: ResponseVndirect = timDoiTuongCoMa(datas, head);
+      if (element.nmVolume === undefined) {
+        throw new Error(`Sai du lieu`);
+      } else {
+        khoiLuong.push(String(element.nmVolume));
+      }
+    }
+  }
+  sheetHelper.ghiDuLieuVaoDay(khoiLuong, 'Tam', hang, 1);
+  console.log('Lấy thông tin thành công');
+}
+
+function timDoiTuongCoMa(danhSach: ResponseVndirect[], tenMa: string): ResponseVndirect {
+  const result = danhSach.find((element) => element.code === tenMa);
+  if (result === undefined) {
+    return {
+      buyVol: 0,
+      sellVol: 0,
+      nmVolume: 0
+    };
+  }
+  return result;
+}
