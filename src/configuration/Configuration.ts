34047d3d0eee22bf9ea1e4d5a902397b638c0e25
hontrang <tang11392@gmail.com>
2024-08-24 13:41:56 +0700

    fix: wrap bởi playwright framework

--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 export class Configuration {
-    static readonly consumerID = process.env.CONSUMERID
-    static readonly consumerSecret = process.env.CONSUMERSECRET
+    static readonly consumerID = 'replaced'
+    static readonly consumerSecret = 'replaced'
     static readonly mode = 'offline';
-    static readonly xlsxInput = "./dw.xlsx";
-    static readonly xlsxOutput = "./build/dw_new.xlsx";
+    static readonly xlsxInput = './dw.xlsx';
+    static readonly xlsxOutput = './build/dw_new.xlsx';
+    static readonly dwClasp = '/Users/hontrang/code/clasp/dw';
+    static readonly mainClasp = '/Users/hontrang/code/clasp/main';
 }
