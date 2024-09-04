1d56365a9dcf2a128d451a8d09a51e3abdb32b2f
hontrang <tang11392@gmail.com>
2024-09-04 16:04:32 +0700

    fix: sửa code nhầm

--- tests/tradingeconomics.ts
+++ tests/tradingeconomics.ts
@@ -3,7 +3,7 @@ import { writeFile } from 'fs';
 import { chromium } from 'playwright-extra';
 
 test('test', async () => {
-  const browser = await chromium.launch({ headless: false })
+  const browser = await chromium.launch({ headless: true })
   const context = await browser.newContext({
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
   })
