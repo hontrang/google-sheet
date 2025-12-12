96bf4906b3b420e9e05bf9029cbbd171fb1bd6f3
hontrang <tang11392@gmail.com>
2025-12-12 18:17:56 +0700

    fix: sửa hàm lấy thông tin chi tiết mã ko hoạt động + xoá ssi api (ko còn hoạt động) + update api vietstock + xoá bun khỏi source

--- tests/utility.test.ts
+++ tests/utility.test.ts
@@ -9,6 +9,6 @@ test.describe('kiểm tra chức năng của DateHelper', () => {
 
   test('kiểm tra hàm [doiDinhDangNgay]', async () => {
     const actual = DateHelper.doiDinhDangNgay('2025-12-18', 'yyyy-MM-dd', 'EEEE yyyy/MM/dd', { locale: 'vi-VN' });
-    expect(actual).toContain("Thứ Năm 2025/12/18");
+    expect(actual).toContain('Thứ Năm 2025/12/18');
   });
 });
